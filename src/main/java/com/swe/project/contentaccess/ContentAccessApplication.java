12ed8d1483aec3c05504a4db024dfa060c0dc8ae
sinedon <111921789+sinedon@users.noreply.github.com>
2026-04-19 18:34:17 -0400

    Partially working Kubernetes

--- src/main/java/com/swe/project/contentaccess/ContentAccessApplication.java
+++ src/main/java/com/swe/project/contentaccess/ContentAccessApplication.java
@@ -1,25 +1,12 @@
 package com.swe.project.contentaccess;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.swe.project.contentaccess.service.TopicService;
 
 @SpringBootApplication(scanBasePackages = "com.swe.project")
 public class ContentAccessApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContentAccessApplication.class, args);
-	}
-
-	@Bean
-    public CommandLineRunner dataLoader(TopicService topicService) {
-        return new CommandLineRunner() {
-			public void run(String... args) throws Exception {
-				topicService.start();
-			}
-		};
-	}	
+    public static void main(String[] args) {
+        SpringApplication.run(ContentAccessApplication.class, args);
+    }
 }
--- src/main/java/com/swe/project/contentaccess/service/TopicService.java
+++ src/main/java/com/swe/project/contentaccess/service/TopicService.java
@@ -1,14 +1,12 @@
 package com.swe.project.contentaccess.service;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.swe.project.contentaccess.dto.CreateTopicRequest;
-import com.swe.project.contentaccess.model.Hotspot;
 import com.swe.project.contentaccess.model.Topic;
 import com.swe.project.contentaccess.repository.TopicRepository;
 
@@ -19,89 +17,6 @@ public class TopicService {
 
     public TopicService(TopicRepository topicRepository) {
         this.topicRepository = topicRepository;
-    }
-
-        private final List<Topic> topics = List.of(
-                new Topic(
-                        "fruits",
-                        "Fruits",
-                        "/images/fruits.jpg",
-                        "Food",
-                        List.of(
-                                new Hotspot(
-                                        "Apple",
-                                        "/images/apple.jpg",
-                                        10.0,
-                                        53.0,
-                                        Map.of("Spanish", "Manzana"),
-                                        Map.of("Spanish", "/audio/spanish_apple.mp3")
-                                ),
-                                new Hotspot(
-                                        "Orange",
-                                        "/images/orange.jpg",
-                                        24.0,
-                                        23.0,
-                                        Map.of("Spanish", "Naranja"),
-                                        Map.of("Spanish", "/audio/spanish_orange.mp3")
-                                ),
-                                new Hotspot(
-                                        "Kiwi",
-                                        "/images/kiwi.jpg",
-                                        67.0,
-                                        35.0,
-                                        Map.of("Spanish", "Kiwi"),
-                                        Map.of("Spanish", "/audio/spanish_kiwi.mp3")
-                                ),
-                                new Hotspot(
-                                        "Raspberry",
-                                        "/images/raspberry.jpg",
-                                        81.0,
-                                        67.0,
-                                        Map.of("Spanish", "Frambuesa"),
-                                        Map.of("Spanish", "/audio/spanish_raspberry.mp3")
-                                )
-                        )
-                ),
-                new Topic(
-                        "vegetables",
-                        "Vegetables",
-                        "/images/vegetables.jpg",
-                        "Food",
-                        List.of(
-                                new Hotspot(
-                                        "Carrot",
-                                        "/images/carrot.jpg",
-                                        52.0,
-                                        37.0,
-                                        Map.of("Spanish", "Zanahoria"),
-                                        Map.of("Spanish", "/audio/spanish_carrot.mp3")
-                                ),
-                                new Hotspot(
-                                        "Garlic",
-                                        "/images/garlic.jpg",
-                                        14.0,
-                                        75.0,
-                                        Map.of("Spanish", "Ajo"),
-                                        Map.of("Spanish", "/audio/spanish_garlic.mp3")
-                                ),
-                                new Hotspot(
-                                        "Potato",
-                                        "/images/potato.jpg",
-                                        82.0,
-                                        52.0,
-                                        Map.of("Spanish", "Papa"),
-                                        Map.of("Spanish", "/audio/spanish_potato.mp3")
-                                )
-                        )
-                )
-        );
-
-    public void start() {
-        System.out.println("\n\n\n Number of topics in repository: " + topicRepository.count());
-        if (topicRepository.count() == 0) {
-            System.out.println("\n printing topics to be saved: " + topics + "\n\n\n");
-            topicRepository.saveAll(topics);
-        }
     }
 
     public List<Topic> getAllTopics() {
